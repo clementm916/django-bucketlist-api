e5196220c2932339b43d6aac90ca1908fc0b119d
Clementm916 <clementm916@gmail.com>
2017-01-29 16:31:43 +0300

    [chore  #13856272] adds tests

---
+++ api_v1/tests/test_items.py
@@ -0,0 +1,51 @@
+import django
+django.setup()
+
+from .. models import Bucketlist, Item
+from . import BaseAPITestCase
+
+
+class TestItem(BaseAPITestCase):
+    def setUp(self):
+        super(TestItem, self).setUp()
+        self.bucket = Bucketlist(name="Play", created_by=self.user)
+        self.bucket.save()
+        self.bucket_id = self.bucket.id
+
+    def test_creating_an_item(self):
+        payload = {'name': 'Play soccer'}
+        request = self.test_client.get(
+            '/api/v1/bucketlists/' + str(self.bucket_id) + '/items/', payload, HTTP_AUTHORIZATION=self.auth, format='json')
+        self.assertEqual(request.status_code, 201)
+
+    def test_editing_an_item(self):
+        # an existing item
+        item = Item(name="Play tabletennis", bucketlist_id=self.bucket)
+        item.save()
+        item_id = item.id
+        payload = {'name': 'Play soccer'}
+
+        request = self.test_client.get(
+            '/api/v1/bucketlists/' + str(self.bucket_id) + '/items/' + str(item_id), payload, HTTP_AUTHORIZATION=self.auth, format='json')
+        self.assertEqual(request.status_code, 201)
+        self.assertIn('Play soccer', request.data)
+        # a non-existent item
+        item_id = 'notthere'
+        request = self.test_client.get(
+            '/api/v1/bucketlists/' + str(self.bucket_id) + '/items/' + str(item_id), payload, HTTP_AUTHORIZATION=self.auth, format='json')
+        self.assertEqual(request.status_code, 404)
+        # an empty name
+
+    def test_deleting_an_item(self):
+        # an existing item
+        item = Item(name="Play tabletennis", bucketlist_id=self.bucket)
+        item.save()
+        item_id = item.id
+        payload = {'name': 'Play soccer'}
+
+        request = self.test_client.get(
+            '/api/v1/bucketlists/' + str(self.bucket_id) + '/items/' + str(item_id), payload, HTTP_AUTHORIZATION=self.auth, format='json')
+        self.assertEqual(request.status_code, 204)
+        self.assertIn('Play soccer', request.data)
+
+        # a non-existent item
